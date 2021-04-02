5a826dd5eaad848a318cb1ae35b5b02ad02bb531
doraemonfighting <doraemonfighting@163.com>
2021-04-02 13:49:04 +0800

    结构体

--- 结构体作函数参数.cpp
+++ 结构体作函数参数.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
-#include<string>
 using namespace std;
+#include<string>
 struct Student
 {
 	string name;
 	int age;
 	int score;
 };
-struct Teacher
+void PrintStudent1(Student s)
 {
-	string id;
-	string name;
-	int age;
-	Student stu;
-};
-int main() {
-	Teacher tea;
-	tea.id = "10000";
-	tea.name = "张三";
-	tea.age = 50;
-	tea.stu.name = "方涛";
-	tea.stu.age = 19;
-	tea.stu.score = 100;
-	cout << " 老师姓名：" << tea.name << " 老师编号：" << tea.id << " 老师年龄："
-		<< tea.age << " 老师辅导的学生姓名：" << tea.name << " 学生年龄："
-		<< tea.stu.age << " 学生成绩：" << tea.stu.score << endl;
+	s.score = 90;
+	cout << "子函数中 学生1姓名：" << s.name << "  年龄：" << s.age << "  分数：" << s.score << endl;
+}
+void PrintStudent2(Student* p)
+{
+	p->age = 21;
+	cout << "子函数中 学生2姓名：" << p->name << "  年龄：" << p->age << "  分数" << p->score << endl;
+}
+int main()
+{
+	Student s1;
+	s1.name = "张三";
+	s1.age = 19;
+	s1.score = 100;
+	Student s2 = { "李四",20,80 };
+	PrintStudent1(s1);
+	cout << "main函数中 学生1姓名：" << s1.name << "  年龄：" << s1.age << "  分数：" << s1.score << endl;
+	PrintStudent2(&s2);
+	cout << "main函数中 学生2姓名：" << s2.name << "  年龄：" << s2.age << "  分数：" << s2.score << endl;
 	system("pause");
 	return 0;
 }
