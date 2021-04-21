5d5cfa75f26cfe1722940c128f5a4750aac9f187
doraemonfighting <doraemonfighting@163.com>
2021-04-21 18:07:50 +0800

    点和圆的位置关系

--- 点和圆位置关系.cpp
+++ 点和圆位置关系.cpp
@@ -73,11 +73,11 @@ int main()
 	{
 		Circle c;
 		Point p;
+		Point p1;
 		int r;
 		cout << "ÇëÊäÈëÔ²µÄ°ë¾¶" << endl;
 		cin >> r;
 		c.setR(r);
-		Point p1;
 		int x1, y1;
 		cout << "ÇëÊäÈëÔ²ÐÄx y×ø±ê" << endl;
 		cin >> x1 >> y1;
